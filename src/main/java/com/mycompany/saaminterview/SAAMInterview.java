993f6c6cb8eac8f93f9a5a6808d478c64fdb2eb5
Diego Trevisan <106852450+diegotrevisancc@users.noreply.github.com>
2024-04-09 11:55:28 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/saaminterview/SAAMInterview.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.saaminterview;
+
+/**
+ *
+ * @author diego
+ */
+public class SAAMInterview {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
